34d306951d282d337af4567201ffe8a9ea589bef
Wyn Price <wynprice999@gmail.com>
2018-10-20 00:02:49 +0100

    Version bump

--- src/main/java/com/wynprice/boneophone/SkeletalBand.java
+++ src/main/java/com/wynprice/boneophone/SkeletalBand.java
@@ -58,7 +58,7 @@ import java.util.Objects;
 public class SkeletalBand {
     public static final String MODID = "skeletalband";
     public static final String NAME = "SkeletalBand";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
 
     public static Logger LOGGER;
 
